28756789b3a2451c90ce64a788e8c82db9196c55
lequangtrieu <trieulqde160447@gmail.com>
2024-02-26 21:20:12 +0700

    up fe CRUDMedicineGroup and fe RDMedicineInfo

--- pharmacy-store-management/src/App.js
+++ pharmacy-store-management/src/App.js
@@ -1,22 +1,16 @@
 import './App.css';
+import {MedicineGroupList} from "./pages/MedicineGroup/MedicineGroupList";
+import {Route, Routes} from "react-router-dom";
+import 'react-toastify/dist/ReactToastify.css';
+import {MedicineInfoList} from "./pages/MedicineInformation/MedicineInfoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+          <Route path="/medicineGroup" element={<MedicineGroupList/>}></Route>
+          <Route path="/medicineInfo" element={<MedicineInfoList/>}></Route>
+      </Routes>
+
   );
 }
 

2fd698abaaa6e61596aaed2e5481269857d21a5b
TayKieu <kieuduongtay@gmail.com>
2024-03-14 22:02:58 +0700

    nhi-tay

--- pharmacy-store-management/src/App.js
+++ pharmacy-store-management/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import {ListCustomer} from "./components/CreateUpdateCustomer/ListCustomer";
+import {ListCustomer} from "./components/customer/ListCustomer";
 import {BrowserRouter as Router,Route, Routes} from "react-router-dom";
 import React from "react";
 import {ToastContainer} from "react-toastify";
+import {DetailCustomer} from "./components/customer/DetailCustomer";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
       <Router>
           <Routes>
               <Route path="/listCustomer" element={<ListCustomer/>}></Route>
+              <Route path="/detail-customer/:customerId" element={<DetailCustomer/>}/>
           </Routes>
+          <ToastContainer></ToastContainer>
       </Router>
     </>
   );

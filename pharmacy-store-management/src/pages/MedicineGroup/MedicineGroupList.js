b69e5e7a9f63c4ec079962ab79107b8f7e60b16b
lequangtrieu <trieulqde160447@gmail.com>
2024-03-14 23:06:57 +0700

    up TrieuLQ

--- pharmacy-store-management/src/pages/MedicineGroup/MedicineGroupList.js
+++ pharmacy-store-management/src/pages/MedicineGroup/MedicineGroupList.js
@@ -3,6 +3,8 @@ import "../MedicineGroup/MedicineGroupList.css"
 import {useEffect, useState} from "react";
 import * as medicineGroupService from "../../utils/InformationService/MedicineGroupManagementService/MedicineGroupService"
 import {toast} from "react-toastify";
+import Header from "../../components/header/Header";
+import Nav from "../../components/nav/Nav";
 
 export function MedicineGroupList() {
     const [medicineGroup, setMedicineGroup] = useState([]);
@@ -150,76 +152,84 @@ export function MedicineGroupList() {
     };
     return (
         <>
-            <div className="container">
-                <h1 className="text-center bg-primary">Nhóm thuốc</h1>
+            <Header/>
+            <section className="main">
+                <Nav />
+                <div className="main-right">
+                    <div className="container">
+                        <h1 className="text-center bg-primary">Nhóm thuốc</h1>
 
-                <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
-                    <legend><h2>Danh sách nhóm thuốc</h2></legend>
-                    <table className="table table-bordered">
-                        <thead>
-                        <tr>
-                            <th className="bg-primary text-light">Mã nhóm thuốc</th>
-                            <th className="bg-primary text-light">Tên nhóm thuốc</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            medicineGroup?.map((item) => (
-                                <tr key={item.medicineGroupId} onClick={() => selectRow(item)} className={medicineGroupId === item.medicineGroupId ? 'table-primary' : ''}>
-                                    <td>{item.medicineGroupId}</td>
-                                    <td>{item.medicineGroupName}</td>
+                        <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
+                            <legend><h2>Danh sách nhóm thuốc</h2></legend>
+                            <table className="table table-bordered">
+                                <thead>
+                                <tr>
+                                    <th className="bg-primary text-light">Mã nhóm thuốc</th>
+                                    <th className="bg-primary text-light">Tên nhóm thuốc</th>
                                 </tr>
-                            ))
-                        }
-                        </tbody>
-                    </table>
-                </fieldset>
+                                </thead>
+                                <tbody>
+                                {
+                                    medicineGroup?.map((item) => (
+                                        <tr key={item.medicineGroupId} onClick={() => selectRow(item)} className={medicineGroupId === item.medicineGroupId ? 'table-primary' : ''}>
+                                            <td>{item.medicineGroupId}</td>
+                                            <td>{item.medicineGroupName}</td>
+                                        </tr>
+                                    ))
+                                }
+                                </tbody>
+                            </table>
+                        </fieldset>
 
 
-                <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
-                    <legend><h2>Thông tin nhóm thuốc</h2></legend>
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="groupId" className="form-label">Mã nhóm thuốc</label>
-                            <input type="text" className="form-control" id="groupId" value={medicineGroupId} onChange={(e) => setMedicineGroupId(e.target.value)} disabled/>
+                        <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
+                            <legend><h2>Thông tin nhóm thuốc</h2></legend>
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="groupId" className="form-label">Mã nhóm thuốc</label>
+                                    <input type="text" className="form-control" id="groupId" value={medicineGroupId} onChange={(e) => setMedicineGroupId(e.target.value)} disabled/>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="groupName" className="form-label">Tên nhóm thuốc</label>
+                                    <input type="text" className="form-control" id="groupName"  value={medicineGroupName} onChange={(e) => setMedicineGroupName(e.target.value)}/>
+                                    {errorMessage && <div className="text-danger">{errorMessage}</div>}
+                                </div>
+                            </form>
+                        </fieldset>
+
+
+                        <div className="my-3" style={{textAlign: 'right'}}>
+                            <button className="btn btn-success" id="addBtn" disabled={disableButton} onClick={() => createNewMedicineGroup()}>
+                                <i className="bi bi-plus-circle"></i> {isAdding ? "Hoàn Thành" : "Thêm"}</button>
+                            <button className="btn btn-custom" id="editBtn" disabled={disableButton} onClick={() => updateMedicineGroupById()}><i className="bi bi-pencil-square"></i> Sửa</button>
+                            <button className="btn btn-danger" id="deleteBtn" onClick={() => confirmDelete()}><i className="bi bi-x-circle"></i> Xóa</button>
+                            <button className="btn btn-primary" id="backBtn" onClick={() => backButton()}><i className="bi bi-arrow-return-left"></i> Trở về</button>
                         </div>
-                        <div className="mb-3">
-                            <label htmlFor="groupName" className="form-label">Tên nhóm thuốc</label>
-                            <input type="text" className="form-control" id="groupName"  value={medicineGroupName} onChange={(e) => setMedicineGroupName(e.target.value)}/>
-                            {errorMessage && <div className="text-danger">{errorMessage}</div>}
-                        </div>
-                    </form>
-                </fieldset>
 
 
-                <div className="my-3" style={{textAlign: 'right'}}>
-                    <button className="btn btn-success" id="addBtn" disabled={disableButton} onClick={() => createNewMedicineGroup()}>
-                        <i className="bi bi-plus-circle"></i> {isAdding ? "Hoàn Thành" : "Thêm"}</button>
-                    <button className="btn btn-custom" id="editBtn" disabled={disableButton} onClick={() => updateMedicineGroupById()}><i className="bi bi-pencil-square"></i> Sửa</button>
-                    <button className="btn btn-danger" id="deleteBtn" onClick={() => confirmDelete()}><i className="bi bi-x-circle"></i> Xóa</button>
-                    <button className="btn btn-primary" id="backBtn" onClick={() => backButton()}><i className="bi bi-arrow-return-left"></i> Trở về</button>
-                </div>
-
-
-                <div className={`modal ${showDeleteModal ? 'show' : ''}`} style={{ display: showDeleteModal ? 'block' : 'none' }}
-                     id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden={!showDeleteModal}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Xóa nhóm thuốc</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}></button>
-                            </div>
-                            <div className="modal-body">
-                                <p>Bạn có chắc chắn muốn xóa nhóm thuốc không</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowDeleteModal(false)}>Close</button>
-                                <button type="button" className="btn btn-danger" onClick={() => deleteMedicineGroupById()}>Delete</button>
+                        <div className={`modal ${showDeleteModal ? 'show' : ''}`} style={{ display: showDeleteModal ? 'block' : 'none' }}
+                             id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden={!showDeleteModal}>
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title">Xóa nhóm thuốc</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <p>Bạn có chắc chắn muốn xóa nhóm thuốc không</p>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowDeleteModal(false)}>Close</button>
+                                        <button type="button" className="btn btn-danger" onClick={() => deleteMedicineGroupById()}>Delete</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-            </div>
+
+            </section>
         </>
     )
 }
--- pharmacy-store-management/src/pages/MedicineInformation/MedicineInfoList.js
+++ pharmacy-store-management/src/pages/MedicineInformation/MedicineInfoList.js
@@ -2,6 +2,8 @@ import {useEffect, useState} from "react";
 import * as medicineService from "../../utils/InformationService/MedicineInformationManagementService/MedicineInformationService";
 import {toast} from "react-toastify";
 import axios from "axios";
+import Header from "../../components/header/Header";
+import Nav from "../../components/nav/Nav";
 
 const attributes = [
     {
@@ -148,114 +150,121 @@ export function MedicineInfoList() {
     }
     return (
         <>
-            <div className="container">
-                <h1 className="text-center bg-primary">Danh sách thuốc</h1>
+            <Header/>
+            <section className="main">
+                <Nav />
+                <div className="main-right">
+                    <div className="container">
+                        <h1 className="text-center bg-primary">Danh sách thuốc</h1>
 
-                <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
-                    <legend><h2>Bộ lọc</h2></legend>
-                    <div className="row">
-                        {errorMessage && <div className="text-danger"><h4>{errorMessage}</h4></div>}
-                        <h3 className="col-lg-2">Lọc theo</h3>
+                        <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
+                            <legend><h2>Bộ lọc</h2></legend>
+                            <div className="row">
+                                {errorMessage && <div className="text-danger"><h4>{errorMessage}</h4></div>}
+                                <h3 className="col-lg-2">Lọc theo</h3>
 
-                        <select name="" id="attributes" className="col-lg-2"
-                                value={attributeSearchField} onChange={e => setAttributeSearchField(e.target.value)}>
-                            <option value=""> - Thuộc tính - </option>
-                            {attributeState.map((attribute) => (
-                                <option value={attribute.id}>{attribute.name}</option>
-                            ))}
+                                <select name="" id="attributes" className="col-lg-2"
+                                        value={attributeSearchField} onChange={e => setAttributeSearchField(e.target.value)}>
+                                    <option value=""> - Thuộc tính - </option>
+                                    {attributeState.map((attribute) => (
+                                        <option value={attribute.id}>{attribute.name}</option>
+                                    ))}
 
 
-                        </select>
+                                </select>
 
-                        <select name="" id="condition" className="col-lg-2 mx-3" disabled={disabledCondition}
-                        value={conditionSearchField} onChange={e => setConditionSearchField(e.target.value)}>
-                            <option value=""> - Điều kiện - </option>
-                            {conditionState.map((condition) => (
-                                <option value={condition}>{condition}</option>
-                            ))}
-                        </select>
+                                <select name="" id="condition" className="col-lg-2 mx-3" disabled={disabledCondition}
+                                        value={conditionSearchField} onChange={e => setConditionSearchField(e.target.value)}>
+                                    <option value=""> - Điều kiện - </option>
+                                    {conditionState.map((condition) => (
+                                        <option value={condition}>{condition}</option>
+                                    ))}
+                                </select>
 
-                        <input type="text" className="col-lg-3 mx-3" onChange={e => setTextSearchField(e.target.value)}/>
-                        <button onClick={() => getMedicineByCondition()} className="col-lg-2 btn btn-primary"><i className="bi bi-search"></i> Lọc kết quả</button>
-                    </div>
-                </fieldset>
+                                <input type="text" className="col-lg-3 mx-3" onChange={e => setTextSearchField(e.target.value)}/>
+                                <button onClick={() => getMedicineByCondition()} className="col-lg-2 btn btn-primary"><i className="bi bi-search"></i> Lọc kết quả</button>
+                            </div>
+                        </fieldset>
 
-                <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
-                    <legend><h2>Danh sách nhóm thuốc</h2></legend>
-                    <div className="table-responsive">
-                        <table className="table table-bordered"  style={{ border: '4px solid black' }}>
-                            <thead>
-                            <tr>
-                                <th className="bg-primary text-light">Mã thuốc</th>
-                                <th className="bg-primary text-light">Nhóm thuốc</th>
-                                <th className="bg-primary text-light">Tên thuốc</th>
-                                <th className="bg-primary text-light">Hoạt chất</th>
-                                <th className="bg-primary text-light">ĐVT</th>
-                                <th className="bg-primary text-light">ĐV QĐ</th>
-                                <th className="bg-primary text-light">Số lượng</th>
-                                <th className="bg-primary text-light">Giá nhập</th>
-                                <th className="bg-primary text-light">Giá sĩ</th>
-                                <th className="bg-primary text-light">Giá lẻ</th>
-                                <th className="bg-primary text-light">% CK</th>
-                                <th className="bg-primary text-light">% LN XC</th>
-                                <th className="bg-primary text-light">% LN XL</th>
-                                <th className="bg-primary text-light">VAT</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-                                medicines?.map((item) => (
-                                    <tr key={item.medicineId} onClick={() => selectRow(item)} className={medicine.medicineId === item.medicineId ? 'table-primary' : ''}>
-                                        <td>{item.medicineId}</td>
-                                        <td>{item.medicineGroup?.medicineGroupName}</td>
-                                        <td>{item.medicineName}</td>
-                                        <td>{item.activeIngredient}</td>
-                                        <td>{item.unit}</td>
-                                        <td>{item.conversionUnit}</td>
-                                        <td>{item.quantity}</td>
-                                        <td>{item.importPrice}$</td>
-                                        <td>{item.wholesalePrice}$</td>
-                                        <td>{item.retailPrice}$</td>
-                                        <td>{item.supplierDiscount}%</td>
-                                        <td>{item.profitMarginWholesale}%</td>
-                                        <td>{item.profitMarginRetail}%</td>
-                                        <td>{item.origin}%</td>
+                        <fieldset className="border border-dark border-3 rounded-3 p-3 mt-4">
+                            <legend><h2>Danh sách nhóm thuốc</h2></legend>
+                            <div className="table-responsive">
+                                <table className="table table-bordered"  style={{ border: '4px solid black' }}>
+                                    <thead>
+                                    <tr>
+                                        <th className="bg-primary text-light">Mã thuốc</th>
+                                        <th className="bg-primary text-light">Nhóm thuốc</th>
+                                        <th className="bg-primary text-light">Tên thuốc</th>
+                                        <th className="bg-primary text-light">Hoạt chất</th>
+                                        <th className="bg-primary text-light">ĐVT</th>
+                                        <th className="bg-primary text-light">ĐV QĐ</th>
+                                        <th className="bg-primary text-light">Số lượng</th>
+                                        <th className="bg-primary text-light">Giá nhập</th>
+                                        <th className="bg-primary text-light">Giá sĩ</th>
+                                        <th className="bg-primary text-light">Giá lẻ</th>
+                                        <th className="bg-primary text-light">% CK</th>
+                                        <th className="bg-primary text-light">% LN XC</th>
+                                        <th className="bg-primary text-light">% LN XL</th>
+                                        <th className="bg-primary text-light">VAT</th>
                                     </tr>
-                                ))
-                            }
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        medicines?.map((item) => (
+                                            <tr key={item.medicineId} onClick={() => selectRow(item)} className={medicine.medicineId === item.medicineId ? 'table-primary' : ''}>
+                                                <td>{item.medicineId}</td>
+                                                <td>{item.medicineGroup?.medicineGroupName}</td>
+                                                <td>{item.medicineName}</td>
+                                                <td>{item.activeIngredient}</td>
+                                                <td>{item.unit}</td>
+                                                <td>{item.conversionUnit}</td>
+                                                <td>{item.quantity}</td>
+                                                <td>{item.importPrice}$</td>
+                                                <td>{item.wholesalePrice}$</td>
+                                                <td>{item.retailPrice}$</td>
+                                                <td>{item.supplierDiscount}%</td>
+                                                <td>{item.profitMarginWholesale}%</td>
+                                                <td>{item.profitMarginRetail}%</td>
+                                                <td>{item.origin}%</td>
+                                            </tr>
+                                        ))
+                                    }
+                                    </tbody>
+                                </table>
 
-                    </div>
-                </fieldset>
-
-                <div className="my-3" style={{textAlign: 'right'}}>
-                    <button className="btn btn-success" id="addBtn" ><i className="bi bi-plus-circle"></i>Thêm</button>
-                    <button className="btn btn-custom" id="editBtn" ><i className="bi bi-pencil-square"></i> Sửa</button>
-                    <button className="btn btn-danger" id="deleteBtn" onClick={() => confirmDelete()}><i className="bi bi-x-circle"></i> Xóa</button>
-                    <button className="btn btn-primary" id="backBtn"><i className="bi bi-arrow-return-left"></i> Trở về</button>
-                </div>
-
-
-                <div className={`modal ${showDeleteModal ? 'show' : ''}`} style={{ display: showDeleteModal ? 'block' : 'none' }}
-                     id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden={!showDeleteModal}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title">Xóa thuốc</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}></button>
                             </div>
-                            <div className="modal-body">
-                                <p>Bạn có chắc chắn muốn xóa thuốc không</p>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowDeleteModal(false)}>Close</button>
-                                <button type="button" className="btn btn-danger" onClick={() => deleteMedicineById()}>Delete</button>
+                        </fieldset>
+
+                        <div className="my-3" style={{textAlign: 'right'}}>
+                            <button className="btn btn-success" id="addBtn" ><i className="bi bi-plus-circle"></i>Thêm</button>
+                            <button className="btn btn-custom" id="editBtn" ><i className="bi bi-pencil-square"></i> Sửa</button>
+                            <button className="btn btn-danger" id="deleteBtn" onClick={() => confirmDelete()}><i className="bi bi-x-circle"></i> Xóa</button>
+                            <button className="btn btn-primary" id="backBtn"><i className="bi bi-arrow-return-left"></i> Trở về</button>
+                        </div>
+
+
+                        <div className={`modal ${showDeleteModal ? 'show' : ''}`} style={{ display: showDeleteModal ? 'block' : 'none' }}
+                             id="deleteModal" tabIndex="-1" aria-labelledby="deleteModalLabel" aria-hidden={!showDeleteModal}>
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <h5 className="modal-title">Xóa thuốc</h5>
+                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => setShowDeleteModal(false)}></button>
+                                    </div>
+                                    <div className="modal-body">
+                                        <p>Bạn có chắc chắn muốn xóa thuốc không</p>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowDeleteModal(false)}>Close</button>
+                                        <button type="button" className="btn btn-danger" onClick={() => deleteMedicineById()}>Delete</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
-            </div>
+            </section>
         </>
     )
 }

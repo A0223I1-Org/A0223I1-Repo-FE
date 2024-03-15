e864746d12766f4f8af954022b9931df13565fde
dinhkhue0303 <you@example.com>
2024-03-15 14:21:52 +0700

    update

--- pharmacy-store-management/src/firebase.js
+++ pharmacy-store-management/src/firebase.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage} from "firebase/storage"
+import {getFirestore} from "firebase/firestore"
+import {getStorage} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBcPaQTEq7mtxmMGxQxVR5480h8G7vFHoQ",
-    authDomain: "uploading-a9e6a.firebaseapp.com",
-    projectId: "uploading-a9e6a",
-    storageBucket: "uploading-a9e6a.appspot.com",
-    messagingSenderId: "989897993690",
-    appId: "1:989897993690:web:f60076de5725066fe2e4e3"
+    apiKey: "AIzaSyCiguIgrL-nqfhLr7BcOzqM60wqF9_m-Ps",
+    authDomain: "fir-upload-405fb.firebaseapp.com",
+    projectId: "fir-upload-405fb",
+    storageBucket: "fir-upload-405fb.appspot.com",
+    messagingSenderId: "671875619581",
+    appId: "1:671875619581:web:0ca507e5d3cc1c9bebfc94"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app);
--- pharmacy-store-management/src/utils/InformationService/EmployeeManagementService/EmployeeUpdate.js
+++ pharmacy-store-management/src/utils/InformationService/EmployeeManagementService/EmployeeUpdate.js
@@ -27,14 +27,15 @@ export function EmployeeUpdate(){
         findById();
     }, []);
     const validateSchema = {
-        employee_name: Yup.string().required(),
-        employee_id: Yup.string().required(),
-        phone_number: Yup.string().required(),
-        address: Yup.string().required(),
-        salary: Yup.string().required(),
-        email: Yup.string().required(),
-        date_start: Yup.string().required(),
-        image: Yup.string().required(),
+        employee_name: Yup.string().required("yêu cầu nhập tên"),
+        employee_id: Yup.string().required("yêu cầu nhập id"),
+        phone_number: Yup.string().required("yêu cầu nhập số điện thoại").matches(/^\d{10}$/,"số điện thoại không hợp lệ"),
+        address: Yup.string().required("yêu cầu nhập địa chỉ"),
+        salary: Yup.string().required("yêu cầu nhập lương"),
+        email: Yup.string().required("yêu cầu nhập email").email("email không hợp lệ"),
+        password: Yup.string().required("yêu cầu nhập mật khẩu"),
+        date_start: Yup.string().required("yêu cầu nhập ngày"),
+        image: Yup.string().required("yêu cầu nhập ảnh"),
     }
     const findById = async () => {
         try {
@@ -61,6 +62,7 @@ export function EmployeeUpdate(){
     }
     return employee.employee_name != "" ? (
         <>
+            <h1 className="bg-primary text-light" style={{display: "flex", justifyContent: "center", marginLeft: "50px", marginRight: "50px"}}>Cập nhật nhân viên</h1>
             <Formik initialValues={employee} onSubmit={(values,{setSubmitting}) => {
                 setSubmitting(false);
                 handleUpdate(values);
@@ -68,60 +70,60 @@ export function EmployeeUpdate(){
                 {
                     ({isSubmitting}) => (
                         <Form>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">Mã nhân viên</label>
                                 <Field type="text" className="form-control" name="employee_id"/>
                                 <ErrorMessage name="employee_id" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">tên nhân viên</label>
                                 <Field type="text" className="form-control" name="employee_name"/>
                                 <ErrorMessage name="employee_name" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">số điện thoại</label>
                                 <Field type="text" className="form-control" name="phone_number"/>
                                 <ErrorMessage name="phone_number" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">ngày vào làm</label>
                                 <Field type="datetime-local" className="form-control" name="date_start"/>
                                 <ErrorMessage name="date_start" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">địa chỉ</label>
                                 <Field type="text" className="form-control" name="address"/>
                                 <ErrorMessage name="address" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">lương</label>
                                 <Field type="number" className="form-control" name="salary"/>
                                 <ErrorMessage name="salary" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">ảnh</label>
                                 <Field type="textl" className="form-control" name="image"/>
                                 <ErrorMessage name="image" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">email</label>
                                 <Field type="text" className="form-control" name="email"/>
                                 <ErrorMessage name="email" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">chức vụ</label>
                                 <Field as="select" name="role_id" className="form-select">
                                     <option value="1">nhân viên</option>
                                     <option value="2">quản lý</option>
                                 </Field>
                             </div>
-                            <div className="mb-3">
+                            <div className="mb-3" style={{marginLeft: "50px", marginRight: "50px"}}>
                                 <label className="form-label">ghi chú</label>
                                 <Field type="text" className="form-control" name="note"/>
                                 <ErrorMessage name="note" component="span" style={{color: "red"}}></ErrorMessage>
                             </div>
                             {
-                                isSubmitting ? <></> : <button type="submit" className="btn btn-primary">update</button>
+                                isSubmitting ? <></> : <button type="submit" className="btn btn-primary" style={{marginLeft: "50px"}}>Cập nhật</button>
                             }
                         </Form>
                     )
